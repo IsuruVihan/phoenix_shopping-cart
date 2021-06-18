3fc02e82d6e8267437b602e83e3ee98837e2f395
SandulR <sandulrenuja@gmail.com>
2021-06-18 10:40:25 +0530

    Added variables for colour, adjusted margins and paddings in NavbarSection

--- src/components/NavbarSection/NavbarTop.tsx
+++ src/components/NavbarSection/NavbarTop.tsx
@@ -6,7 +6,7 @@ const NavbarTop: React.FC = () => {
     return (
         <>
             <Navbar expand="lg" className="navbar-top py-0 mx-2 my-2 ms-lg-auto my-lg-0">
-                <span className="phone">
+                <span className="me-3 ms-auto">
                     <FiPhone className="icon"/>
                     <a href="tel:+94779510260" className="text-secondary text-decoration-none">+94779 510 260</a>
                 </span>
@@ -22,12 +22,12 @@ const NavbarTop: React.FC = () => {
                             <span className="text-black-50">My Account</span>
                         </Nav.Link>
                     </Nav>
-                    <span>
+                    <span className="ms-2 me-0">
                         <Button id="btnRegister"
-                                className="btn-outline-success text-success bg-white mx-2 rounded-1 ms-3">
+                                className="bg-white mx-2 rounded-1 btn-outline">
                             Register
                         </Button>
-                        <Button id="btnLogin" className="bg-white text-success px-2 mx-1 border-0 rounded-1">
+                        <Button id="btnLogin" className="bg-white px-2 mx-1 border-0 rounded-1">
                             Login
                         </Button>
                     </span>
