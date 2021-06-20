324eb13e1249946882e3af3505a89b4c208ed752
SandulR <sandulrenuja@gmail.com>
2021-06-20 09:26:41 +0530

    Added NavbarMain section and updated NavbarSection styles

--- src/components/NavbarSection/NavbarSection.tsx
+++ src/components/NavbarSection/NavbarSection.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import NavbarTop from "./NavbarTop";
+import NavbarMain from "./NavbarMain";
 
 const NavbarSection: React.FC = () => {
     return (
         <>
-            <Row className="navbar-section my-0">
-                <Col className="navbar-top">
+            <Row>
+                <Col className="px-0">
                     <NavbarTop/>
                 </Col>
             </Row>
+
+            <Row className="justify-content-center navbar-shadow">
+                <Col xs={11} md={10} lg={10} className="">
+                    <NavbarMain/>
+                </Col>
+            </Row>
+
         </>
     );
 };
