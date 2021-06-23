5a54fe18312a5d3828a13c891d63395c1d0b3b2c
azeimtiaz <imtiaz.azma@gmail.com>
2021-06-23 15:41:58 +0530

    Form validation

--- src/views/LogIn.tsx
+++ src/views/LogIn.tsx
@@ -1,10 +1,12 @@
-import React, {FC, useState} from 'react';
-import {Col, Container, Row, Form, Button} from "react-bootstrap";
+import React, {ChangeEvent, useState} from 'react';
+import {Col, Container, Row, Form, Button, FormControl} from "react-bootstrap";
 import Grid from '@material-ui/core/Grid';
 import AlternateEmail from '@material-ui/icons/AlternateEmail';
 import Lock from '@material-ui/icons/Lock';
 import TextField from '@material-ui/core/TextField';
-import {makeStyles} from "@material-ui/core";
+import {IconButton, Input, InputAdornment, InputLabel, makeStyles} from "@material-ui/core";
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,14 +15,41 @@ const useStyles = makeStyles((theme) => ({
             width: '25ch',
         },
     },
+    margin: {
+        margin: theme.spacing(1),
+    },
+    textField: {
+        width: '25ch',
+    },
 }));
 
-const LogIn: FC = () => {
+const LogIn: React.FC = () => {
     const classes = useStyles();
     const [inputInvalid, setInputInvalid] = useState('');
     const [userEmail, setUserEmail] = useState('');
-    const handleChange = (event) => {
-        setName(event.target.value);
+    const [userPwd, setUserPwd] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleEmailInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserEmail(event.target.value);
+
+        if(userEmail === '' || userEmail === null) {
+            setInputInvalid('Please enter a valid input!');
+            return;
+        }
+        setInputInvalid('');
+    };
+
+    const handlePwdInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserPwd(event.target.value);
+    };
+
+    const handleClickShowPassword = () => {
+        setShowPassword(true);
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
     };
 
     return (
@@ -39,15 +68,36 @@ const LogIn: FC = () => {
                                 <TextField id="input-with-icon-grid"
                                            value={userEmail}
                                            label="Email"
-                                           helperText={inputInvalid} onChange={handleChange}/>
+                                           helperText={inputInvalid}
+                                           onChange={handleEmailInput}/>
                             </Grid>
                         </Grid>
                         <Grid container spacing={2} alignItems="flex-end">
                             <Grid item>
-                                <Lock />
+                                <Lock style={{ fontSize: 45 }}/>
                             </Grid>
                             <Grid item>
-                                <TextField id="input-with-icon-grid" label="With a grid" helperText="Incorrect entry."/>
+                                <FormControl className={classes.root}>
+                                    <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                                    <Input
+                                        id="standard-adornment-password"
+                                        type={showPassword ? 'text' : 'password'}
+                                        value={userPwd}
+                                        onChange={handlePwdInput}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                >
+                                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                    />
+                                </FormControl>
+                                {/*<TextField id="input-with-icon-grid" label="With a grid" helperText="Incorrect entry."/>*/}
                             </Grid>
                         </Grid>
                     </Col>
