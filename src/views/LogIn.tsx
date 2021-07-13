d5ce0c56450342a957283cd6e2c6ff7c1c1f4083
azeimtiaz <imtiaz.azma@gmail.com>
2021-07-13 07:19:36 +0530

    New UI for Login and Register

--- src/views/LogIn.tsx
+++ src/views/LogIn.tsx
@@ -1,21 +1,18 @@
 import React, {useState} from 'react';
 import {Col, Container, Row, Form, Button} from "react-bootstrap";
 import Grid from '@material-ui/core/Grid';
-import AlternateEmail from '@material-ui/icons/AlternateEmail';
-import Lock from '@material-ui/icons/Lock';
-import {IconButton, Input, InputAdornment, InputLabel, makeStyles} from "@material-ui/core";
+import {FormHelperText, IconButton, Input, InputAdornment, InputLabel, makeStyles} from "@material-ui/core";
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import {ErrorOutline} from "@material-ui/icons";
-import {red} from "@material-ui/core/colors";
 import {NavLink} from "react-router-dom";
+import GroceryBag from "../assets/images/grocery-bag-5.png";
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
-            width: '25ch',
+            width: '15ch',
         },
     },
 }));
@@ -23,17 +20,18 @@ const useStyles = makeStyles((theme) => ({
 const LogIn: React.FC = () => {
     const classes = useStyles();
     const [invalidEmail, setInvalidEmail] = useState(false);
+    const [incorrectPwd, setIncorrectPwd] = useState(false);
     const [userEmail, setUserEmail] = useState('');
     const [userPwd, setUserPwd] = useState('');
     const [showPassword, setShowPassword] = useState(false);
 
     const handleEmailInput = (email: string) => {
-        if(email === '' || email === null) {
-            setInvalidEmail(true);
-            return;
-        }
-        setInvalidEmail(false);
         setUserEmail(email);
+        if (email === '' || email === null) {
+            setInvalidEmail(true);
+        } else {
+            setInvalidEmail(false);
+        }
     };
 
     const handlePwdInput = (password: string) => {
@@ -54,73 +52,70 @@ const LogIn: React.FC = () => {
     };
 
     return (
-        <Container fluid className="login-section px-lg-1 px-sm-2 px-xl-1 mt-3 mb-5">
+        <Container fluid className="px-lg-1 px-sm-2 px-xl-1 mt-5 mb-5">
             <Row>
-                <Col xs={12} className="my-3">
-                    <h5 className="text-center py-2">Log In to Your Account</h5>
-                    <hr className="mb-2 mx-3"/>
+                <Col className="signup-img" xs={12}  md={12} lg={6}>
+                    <img className="w-100" src={GroceryBag} alt="Grocery bag"/>
                 </Col>
-                <Form className={classes.root} noValidate autoComplete="off">
-                    <Col xs={12} className="input-section">
-                        <Grid container spacing={2} alignItems="center">
-                            <Grid item>
-                                <AlternateEmail style={{ fontSize: 40 }}/>
-                            </Grid>
-                            <Grid item>
-                                <InputLabel htmlFor="standard-adornment-password" >Email</InputLabel>
-                                <Input
-                                    id="input-with-icon-grid"
-                                    style={{width: 250, color: '#424242'}}
-                                    type='email'
-                                    value={userEmail}
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                                        handleEmailInput(event.target.value)}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            {invalidEmail ?
-                                                <ErrorOutline 
-                                                    style={{ fontSize: 25,  color: red[500], cursor: "pointer"}}/>
-                                                : null}
-                                        </InputAdornment>
-                                    }
-                                />
-                            </Grid>
-                        </Grid>
-                    </Col>
-                    <Col xs={12} className="input-section">
-                        <Grid container spacing={2} alignItems="flex-end">
-                            <Grid item>
-                                <Lock style={{ fontSize: 40 }}/>
-                            </Grid>
-                            <Grid item>
-                                    <InputLabel htmlFor="standard-adornment-password" >Password</InputLabel>
+                <Col className="login-section mx-auto" xs={12}  md={12} lg={6}>
+                    <Row>
+                        <Col xs={12} className="my-3">
+                            <h5 className="text-center py-2">Log In to Your Account</h5>
+                            <hr className="mx-3"/>
+                        </Col>
+                        <Form className={classes.root} noValidate autoComplete="off">
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="center">
+                                    <InputLabel htmlFor="standard-adornment-required">Email</InputLabel>
                                     <Input
                                         id="input-with-icon-grid"
+                                        style={{width: 250, color: '#424242'}}
+                                        type='email'
+                                        value={userEmail}
+                                        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                            handleEmailInput(event.target.value)}/>
+                                    {invalidEmail ?
+                                        <FormHelperText id="component-helper-text">
+                                            <label className="text-danger">Please enter your email</label>
+                                        </FormHelperText>
+                                        : null}
+                                </Grid>
+                            </Col>
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="flex-end">
+                                    <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                                    <Input
+                                        id="input-with-icon-grid"
+                                        style={{width: 250, color: '#424242'}}
                                         type={showPassword ? 'text' : 'password'}
                                         value={userPwd}
                                         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
                                             handlePwdInput(event.target.value)}
-                                        endAdornment={
-                                            <InputAdornment position="end">
-                                                <IconButton
-                                                    aria-label="toggle password visibility"
-                                                    onClick={handleClickShowPassword}
-                                                    onMouseDown={handleMouseDownPassword}>
-                                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                                </IconButton>
-                                            </InputAdornment>
-                                        }
-                                    />
-                            </Grid>
-                        </Grid>
-                    </Col>
-                    <Col className="mt-3 mb-4 text-center">
-                        <Button className="login-button" type="submit">Log In</Button>
-                    </Col>
-                    <Col xs={12} className="my-3">
-                        <p className="text-center">Don't have an account? <NavLink to="/Register">Register</NavLink></p>
-                    </Col>
-                </Form>
+                                        endAdornment={<InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}>
+                                                {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                            </IconButton>
+                                        </InputAdornment>}/>
+                                    {incorrectPwd ?
+                                        <FormHelperText id="component-helper-text">
+                                            <label className="text-danger">Incorrect Password
+                                                characters</label>
+                                        </FormHelperText>
+                                        : null}
+                                </Grid>
+                            </Col>
+                            <Col className="mt-3 mb-4 text-center">
+                                <Button className="login-button rounded-pill" type="submit">Log In</Button>
+                            </Col>
+                            <Col xs={12} className="my-3">
+                                <p className="text-center">Don't have an account? <NavLink to="/Register">Register</NavLink></p>
+                            </Col>
+                        </Form>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     );
--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -1,22 +1,18 @@
 import React, {useState} from 'react';
 import {IUser} from '../Types/CartTypes';
-import {Col, Container, Row, Form, Button} from "react-bootstrap";
+import {Col, Container, Row, Form, Button, OverlayTrigger, Tooltip} from "react-bootstrap";
 import Grid from '@material-ui/core/Grid';
-import AlternateEmail from '@material-ui/icons/AlternateEmail';
-import Lock from '@material-ui/icons/Lock';
-import {IconButton, Input, InputAdornment, InputLabel, makeStyles} from "@material-ui/core";
-import Visibility from '@material-ui/icons/Visibility';
+import {AlternateEmail, Lock, Visibility, VisibilityOff, ErrorOutline, Phone} from '@material-ui/icons';
+import {FormHelperText, IconButton, Input, InputAdornment, InputLabel, makeStyles, TextField} from "@material-ui/core";
 import PersonIcon from '@material-ui/icons/Person';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import {ErrorOutline} from "@material-ui/icons";
-import {red} from "@material-ui/core/colors";
 import {Link} from "react-router-dom";
+import GroceryBag from "../assets/images/grocery-bag-5.png";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
-            width: '25ch',
+            width: '15ch',
         },
     },
 }));
@@ -28,45 +24,56 @@ const Registration: React.FC = () => {
 
     const [newUserName, setNewUserName] = useState('');
     const [userEmail, setUserEmail] = useState('');
+    const [userMobile, setUserMobile] = useState('');
     const [userPwd, setUserPwd] = useState('');
 
     const [invalidUserName, setInvalidUserName] = useState(false);
     const [invalidEmail, setInvalidEmail] = useState(false);
+    const [invalidMobile, setInvalidMobile] = useState(false);
     const [invalidPassword, setInvalidPassword] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
 
     const handleUserNameInput = (userName: string) => {
-        if(userName === '' || userName === null) {
-            setInvalidUserName(true);
-            return;
-        }
-        setInvalidUserName(false);
         setNewUserName(userName);
+        if (userName === '' || userName === null) {
+            setInvalidUserName(true);
+        } else {
+            setInvalidUserName(false);
+        }
     }
 
     const handleEmailInput = (email: string) => {
-        if(email === '' || email === null) {
-            setInvalidEmail(true);
-            return;
-        }
-        setInvalidEmail(false);
         setUserEmail(email);
+        if (email === '' || email === null) {
+            setInvalidEmail(true);
+        } else {
+            setInvalidEmail(false);
+        }
     };
 
-    const handlePwdInput = (password: string) => {
-        if(password.length < 8) {
-            setInvalidPassword(true);
-            return;
+    const handleMobileInput = (mobile: string) => {
+        setUserMobile(mobile);
+        if (mobile === '' || mobile === null) {
+            setInvalidMobile(true);
+        } else {
+            setInvalidMobile(false);
         }
-        setInvalidPassword(false);
+    };
+
+
+    const handlePwdInput = (password: string) => {
         setUserPwd(password);
+        if (password.length < 8) {
+            setInvalidPassword(true);
+        } else {
+            setInvalidPassword(false);
+        }
     };
 
     const handleClickShowPassword = () => {
         if (!showPassword) {
             setShowPassword(true);
-        }
-        else {
+        } else {
             setShowPassword(false);
         }
     };
@@ -76,108 +83,135 @@ const Registration: React.FC = () => {
     };
 
     const createNewUser = () => {
-        const newUser : IUser = {name: newUserName, email: userEmail, password: userPwd};
-        const listOfUsers : IUser[] = users.slice();
+        const newUser: IUser = {name: newUserName, email: userEmail, password: userPwd};
+        const listOfUsers: IUser[] = users.slice();
         listOfUsers.push(newUser);
         setUsers(listOfUsers);
     }
 
     return (
-        <Container fluid className="login-section px-lg-1 px-sm-2 px-xl-1 mt-3 mb-5">
+        <Container fluid className="px-lg-1 px-sm-2 px-xl-1 mt-5 mb-5">
             <Row>
-                <Col xs={12} className="my-3">
-                    <h5 className="text-center py-2">Sign Up and Start Shopping</h5>
-                    <hr className="mb-1 mx-3"/>
+                <Col className="signup-img" xs={12}  md={12} lg={6}>
+                    <img className="w-100" src={GroceryBag} alt="Grocery bag"/>
                 </Col>
-                <Form className={classes.root} autoComplete="off" onSubmit={createNewUser}>
-                    <Col xs={12} className="input-section">
-                        <Grid container spacing={2} alignItems="center">
-                            <Grid item>
-                                <PersonIcon style={{ fontSize: 40 }}/>
-                            </Grid>
-                            <Grid item>
-                                <InputLabel htmlFor="standard-adornment-password" >Full Name</InputLabel>
-                                <Input
-                                    id="input-with-icon-grid"
-                                    style={{width: 250, color: '#424242'}}
-                                    type='text'
-                                    value={newUserName}
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                                        handleUserNameInput(event.target.value)}
-                                    endAdornment={
-                                        <InputAdornment position="end" title="Please enter your name">
-                                            {invalidUserName ?
-                                                <ErrorOutline
-                                                    style={{ fontSize: 25,  color: red[500], cursor: "pointer"}}/>
-                                                : null}
-                                        </InputAdornment>
-                                    }
-                                />
-                            </Grid>
-                        </Grid>
-                    </Col>
-                    <Col xs={12} className="input-section">
-                        <Grid container spacing={2} alignItems="center">
-                            <Grid item>
-                                <AlternateEmail style={{ fontSize: 40 }}/>
-                            </Grid>
-                            <Grid item>
-                                <InputLabel htmlFor="standard-adornment-password" >Email</InputLabel>
-                                <Input
-                                    id="input-with-icon-grid"
-                                    style={{width: 250, color: '#424242'}}
-                                    type='email'
-                                    value={userEmail}
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                                        handleEmailInput(event.target.value)}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            {invalidEmail ?
-                                                <ErrorOutline
-                                                    style={{ fontSize: 25,  color: red[500], cursor: "pointer"}}/>
-                                                : null}
-                                        </InputAdornment>
-                                    }
-                                />
-                            </Grid>
-                        </Grid>
-                    </Col>
-                    <Col xs={12} className="input-section">
-                        <Grid container spacing={2} alignItems="flex-end">
-                            <Grid item>
-                                <Lock style={{ fontSize: 40 }}/>
-                            </Grid>
-                            <Grid item>
-                                <InputLabel htmlFor="standard-adornment-password" >Password</InputLabel>
-                                <Input
-                                    id="input-with-icon-grid"
-                                    type={showPassword ? 'text' : 'password'}
-                                    value={userPwd}
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                                        handlePwdInput(event.target.value)}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}>
-                                                {showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                />
-                            </Grid>
-                        </Grid>
-                    </Col>
-                    <Col className="mt-3 mb-4 text-center">
-                        <Button className="login-button" type="submit">
-                            Register
-                        </Button>
-                    </Col>
-                    <Col xs={12} className="my-3">
-                        <p className="text-center">Already have an account? <Link to="#">Sign In</Link></p>
-                    </Col>
-                </Form>
+                <Col className="login-section mx-auto" xs={12}  md={12} lg={6}>
+                    <Row>
+                        <Col xs={12} className=" my-3">
+                            <h5 className="text-center py-2">Sign Up and Start Shopping</h5>
+                            <hr className="mx-3"/>
+                        </Col>
+                        <Form className={classes.root} noValidate autoComplete="off" onSubmit={createNewUser}>
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item>
+                                        <PersonIcon style={{fontSize: 30}}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <InputLabel htmlFor="standard-adornment-required">First Name</InputLabel>
+                                        <Input
+                                            id="input-with-icon-grid"
+                                            style={{width: 250, color: '#424242'}}
+                                            type='text'
+                                            value={newUserName}
+                                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                                handleUserNameInput(event.target.value)}/>
+                                        {invalidUserName ?
+                                            <FormHelperText id="component-helper-text">
+                                                <label className="text-danger">Please enter your name</label>
+                                            </FormHelperText>
+                                            : null}
+                                    </Grid>
+                                </Grid>
+                            </Col>
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item>
+                                        <AlternateEmail style={{fontSize: 30}}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <InputLabel htmlFor="standard-adornment-required">Email</InputLabel>
+                                        <Input
+                                            id="input-with-icon-grid"
+                                            style={{width: 250, color: '#424242'}}
+                                            type='email'
+                                            value={userEmail}
+                                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                                handleEmailInput(event.target.value)}/>
+                                        {invalidEmail ?
+                                            <FormHelperText id="component-helper-text">
+                                                <label className="text-danger">Please enter your email</label>
+                                            </FormHelperText>
+                                            : null}
+                                    </Grid>
+                                </Grid>
+                            </Col>
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="center">
+                                    <Grid item>
+                                        <Phone style={{fontSize: 30}}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <InputLabel htmlFor="standard-adornment-required">Mobile No.</InputLabel>
+                                        <Input
+                                            id="input-with-icon-grid"
+                                            style={{width: 250, color: '#424242'}}
+                                            type='text'
+                                            defaultValue='+94'
+                                            inputProps={{maxLength: 10,}}
+                                            value={userMobile}
+                                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                                handleMobileInput(event.target.value)}/>
+                                        {invalidMobile ?
+                                            <FormHelperText id="component-helper-text">
+                                                <label className="text-danger">Please enter your mobile number</label>
+                                            </FormHelperText>
+                                            : null}
+                                    </Grid>
+                                </Grid>
+                            </Col>
+                            <Col xs={12} className="input-section">
+                                <Grid container spacing={2} alignItems="flex-end">
+                                    <Grid item>
+                                        <Lock style={{fontSize: 30}}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                                        <Input
+                                            id="input-with-icon-grid"
+                                            style={{width: 250, color: '#424242'}}
+                                            type={showPassword ? 'text' : 'password'}
+                                            value={userPwd}
+                                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                                handlePwdInput(event.target.value)}
+                                            endAdornment={<InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}>
+                                                    {showPassword ? <Visibility/> : <VisibilityOff/>}
+                                                </IconButton>
+                                            </InputAdornment>}/>
+                                        {invalidPassword ?
+                                            <FormHelperText id="component-helper-text">
+                                                <label className="text-danger">Password should contain minimum 8
+                                                    characters</label>
+                                            </FormHelperText>
+                                            : null}
+                                    </Grid>
+                                </Grid>
+                            </Col>
+                            <Col className="mt-3 mb-4 text-center">
+                                <Button className="login-button rounded-pill" type="submit">
+                                    Register
+                                </Button>
+                            </Col>
+                            <Col xs={12} className="my-3">
+                                <p className="text-center">Already have an account? <Link to="/Login">Sign In</Link></p>
+                            </Col>
+                        </Form>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     );
