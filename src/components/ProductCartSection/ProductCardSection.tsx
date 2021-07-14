34112d31eb260dc78d049dd1560eb6f6e0820e4b
Isuru Soysa <isurumsoysa@gmail.com>
2021-07-14 14:43:53 +0530

    Basic Filtering Complete

--- src/components/ProductCartSection/ProductCardSection.tsx
+++ src/components/ProductCartSection/ProductCardSection.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import ProductCard from "./ProductCard";
 
-const ProductCardsSection: React.FC = () => {
+type productCardTypes = {
+    categoryName : string
+}
+
+function ProductCardsSection (props:productCardTypes){
     return (
-        <Row className="mx-0" >
+        <Row className="mx-0 pt-4" >
             <Col xl={12} sm={12} xs={12} className="product-cards-selection  px-0">
                 <Row className="ms-4">
                     <Col className="ms-2 ps-2">
-                        <label className="cat-title">Vegetables</label>
+                        <label className="cat-title">{props.categoryName}</label>
                     </Col>
                 </Row>
                 <Row className="mx-xl-4 mx-lg-4 mx-0">
--- src/components/ProductCartSection/ProductCartSectionDOM.tsx
+++ src/components/ProductCartSection/ProductCartSectionDOM.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container} from "react-bootstrap";
 import SearchBarSection from "./SearchBarSection";
 import ProductCategorySection from "./ProductCategorySection";
 import ProductCardsSection from "./ProductCardSection";
 
 const ProductCartSectionDOM: React.FC = () => {
+    const [categoryName,setCategorryName] = useState<string>('All');
+
+    const clickedCategory = (catClicked:string) => {
+        setCategorryName(catClicked);
+    }
+
     return (
         <Container className="product-cart-cont  px-xl-4 px-lg-4 px-0 ">
             <SearchBarSection/>
-            <ProductCategorySection/>
-            <ProductCardsSection/>
+            <ProductCategorySection clickedCategory={clickedCategory} />
+            {(categoryName == 'All') ?
+                <React.Fragment>
+                <ProductCardsSection categoryName={"Grocery"}/>
+                <ProductCardsSection categoryName={"Pharmacy"}/>
+                <ProductCardsSection categoryName={"Food"}/>
+                <ProductCardsSection categoryName={"Electronic"}/>
+                </React.Fragment>
+             : (categoryName == 'Grocery') ?
+                    <ProductCardsSection categoryName={"Grocery"}/>
+             : (categoryName == 'Pharmacy') ?
+                    <ProductCardsSection categoryName={"Pharmacy"}/>
+             : (categoryName == 'Food') ?
+                    <ProductCardsSection categoryName={"Food"}/>
+             :      <ProductCardsSection categoryName={"Electronic"}/>
+            }
+
         </Container>
     )
 }
