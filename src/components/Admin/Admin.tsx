5fcd91daf57650f64701759d6b870910b529d2e9
IsuruVihan <isuruvihan@gmail.com>
2021-07-14 08:37:51 +0530

    Set-up temporary Admin page

---
+++ src/components/Admin/Admin.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Container} from "react-bootstrap";
+
+const Admin: FC = () => {
+  return (
+    <Container>
+      Admin
+    </Container>
+  );
+}
+
+export default Admin;
