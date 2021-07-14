0a1c7d7a7a80e4beaaf87748ef59ec54e29b00a4
IsuruVihan <isuruvihan@gmail.com>
2021-07-14 10:34:06 +0530

    Set-up Orders and Products tabs

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,10 +1,35 @@
 import React, {FC} from 'react';
-import {Container} from "react-bootstrap";
+import {Col, Container, Nav, Row, Tab} from "react-bootstrap";
+import Products from "./Products/Products";
+import Orders from "./Orders/Orders";
 
 const Admin: FC = () => {
   return (
-    <Container>
-      Admin
+    <Container className="admin pt-sm-5">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col sm={2}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item className="text-center nav-item">
+                <Nav.Link eventKey="products">Products</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="text-center nav-item">
+                <Nav.Link eventKey="orders">Orders</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={10}>
+            <Tab.Content>
+              <Tab.Pane eventKey="products">
+                <Products />
+              </Tab.Pane>
+              <Tab.Pane eventKey="orders">
+                <Orders />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </Container>
   );
 }
---
+++ src/components/Admin/Products/Products.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Container} from "react-bootstrap";
+
+const Products: FC = () => {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default Products;
