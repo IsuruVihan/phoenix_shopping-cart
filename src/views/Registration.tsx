e66e86eb6473aaaa162a08ead7ace98c21c719df
azeimtiaz <imtiaz.azma@gmail.com>
2021-07-16 00:51:04 +0530

    increased input field width, added prefix to mobile no

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -148,8 +148,9 @@ const Registration: React.FC = () => {
                       style={{width: '250',}}
                       type='text'
                       defaultValue='+94'
-                      inputProps={{maxLength: 10,}}
+                      inputProps={{maxLength: 9,}}
                       value={userMobile}
+                      startAdornment={<InputAdornment position="start">+94 </InputAdornment>}
                       onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
                         handleMobileInput(event.target.value)}/>
                     {invalidMobile ?
