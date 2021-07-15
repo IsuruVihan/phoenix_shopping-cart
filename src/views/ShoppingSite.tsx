fa7925a962b36a8a2ff4190d225ca25ad20050c1
SandulR <sandulrenuja@gmail.com>
2021-07-15 08:42:31 +0530

    Added order-confirm alert in checkout page and implemented scroll to top when mounting homepage component

--- src/views/ShoppingSite.tsx
+++ src/views/ShoppingSite.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import WelcomeCarousel from "../components/WelcomeCarousel";
 import {Col, Container, Row} from "react-bootstrap";
 import ProductCartSectionDOM from "../components/ProductCartSection/ProductCartSectionDOM";
 
 const ShoppingSite: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <Container fluid className="px-0">
-        <Row className="mx-0">
-          <Col xs={12} className="px-0">
-            <WelcomeCarousel/>
-          </Col>
-          <Col xs={12} className="px-0">
-                <ProductCartSectionDOM/>
-          </Col>
-        </Row>
+      <Row className="mx-0">
+        <Col xs={12} className="px-0">
+          <WelcomeCarousel/>
+        </Col>
+        <Col xs={12} className="px-0">
+          <ProductCartSectionDOM/>
+        </Col>
+      </Row>
     </Container>
   );
 }
