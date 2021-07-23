f93895612ccb021f0c7fd4243a407b33b2a452f1
Sandul Renuja <sandulrenuja@gmail.com>
2021-07-24 02:07:54 +0530

    Added support for redux-devtools extension

--- src/state/Store.tsx
+++ src/state/Store.tsx
@@ -1,9 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import Reducers from './reducers';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from "redux-devtools-extension";
 
 export const Store = createStore(
   Reducers,
   {},
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 );
