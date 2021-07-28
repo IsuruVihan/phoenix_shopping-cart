fefeb5b45524df1204df42b2303cf93d2d3104d1
Sandul Renuja <sandulrenuja@gmail.com>
2021-07-28 09:07:47 +0530

    Fix navbar category dropdown
    Add categories to dropdown in navbar, modify styles and implement scroll to relevant category on click

--- src/components/ProductCartSection/CategoryItem.tsx
+++ src/components/ProductCartSection/CategoryItem.tsx
@@ -1,31 +1,33 @@
-import React  from 'react';
+import React from 'react';
 import {Col, Container, Image, Row} from "react-bootstrap";
 
 
 type CategoryItemProps = {
-    image : string
-    catName : string
-    productCategoryTypes : (catName:string) => void;
+  image: string
+  catName: string
+  productCategoryTypes: (catName: string) => void;
 }
 
-function CategoryItem  (props:CategoryItemProps) {
-    return (
-            // <Col className="category-cont justify-content-center px-0 py-2">
-        <Container className="category-cont justify-content-center mx-lg-2 mx-1  px-0 py-4">
-                <Row className="text-center">
-                    <Col onClick={() => {props.productCategoryTypes(props.catName)}}>
-                        <Image src={props.image}
-                               width = "50%"
-                               height="auto"
-                               className="cat-img"
-                        />
-                    </Col>
-                </Row>
-                <Row className="text-center py-2">
-                    <Col className="category-name"><label>{props.catName}</label> </Col>
-                </Row>
-        </Container>
-    )
+function CategoryItem(props: CategoryItemProps) {
+  return (
+    // <Col className="category-cont justify-content-center px-0 py-2">
+    <Container className="category-cont justify-content-center mx-lg-2 mx-1  px-0 py-4">
+      <Row className="text-center">
+        <Col id={props.catName} onClick={() => {
+          props.productCategoryTypes(props.catName)
+        }}>
+          <Image src={props.image}
+                 width="50%"
+                 height="auto"
+                 className="cat-img"
+          />
+        </Col>
+      </Row>
+      <Row className="text-center py-2">
+        <Col className="category-name"><label>{props.catName}</label> </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default CategoryItem;
--- src/components/ProductCartSection/ProductCartSectionDOM.tsx
+++ src/components/ProductCartSection/ProductCartSectionDOM.tsx
@@ -5,32 +5,32 @@ import ProductCategorySection from "./ProductCategorySection";
 import ProductCardsSection from "./ProductCardSection";
 
 const ProductCartSectionDOM: React.FC = () => {
-    const [categoryName,setCategoryName] = useState<string>('All');
-        const clickedCategory = (catClicked:string) => {
-        setCategoryName(catClicked);
-    }
+  const [categoryName, setCategoryName] = useState<string>('All');
+  const clickedCategory = (catClicked: string) => {
+    setCategoryName(catClicked);
+  }
 
-    return (
-        <Container className="product-cart-cont  px-xl-4 px-lg-4 px-0 ">
-            <SearchBarSection/>
-            <ProductCategorySection clickedCategory={clickedCategory} />
-            {(categoryName === 'All') ?
-                <React.Fragment>
-                  <ProductCardsSection categoryName={"Grocery"}/>
-                  <ProductCardsSection categoryName={"Pharmacy"}/>
-                  <ProductCardsSection categoryName={"Food"}/>
-                  <ProductCardsSection categoryName={"Electronic"}/>
-                </React.Fragment>
-             : (categoryName === 'Grocery') ?
-                    <ProductCardsSection categoryName={"Grocery"}/>
-             : (categoryName === 'Pharmacy') ?
-                    <ProductCardsSection categoryName={"Pharmacy"}/>
-             : (categoryName === 'Food') ?
-                    <ProductCardsSection categoryName={"Food"}/>
-             :      <ProductCardsSection categoryName={"Electronic"}/>
-            }
-        </Container>
-    )
+  return (
+    <Container className="product-cart-cont  px-xl-4 px-lg-4 px-0 ">
+      <SearchBarSection/>
+      <ProductCategorySection clickedCategory={clickedCategory}/>
+      {(categoryName === 'All') ?
+        <React.Fragment>
+          <ProductCardsSection categoryName={"Grocery"}/>
+          <ProductCardsSection categoryName={"Pharmacy"}/>
+          <ProductCardsSection categoryName={"Food"}/>
+          <ProductCardsSection categoryName={"Electronic"}/>
+        </React.Fragment>
+        : (categoryName === 'Grocery') ?
+          <ProductCardsSection categoryName={"Grocery"}/>
+          : (categoryName === 'Pharmacy') ?
+            <ProductCardsSection categoryName={"Pharmacy"}/>
+            : (categoryName === 'Food') ?
+              <ProductCardsSection categoryName={"Food"}/>
+              : <ProductCardsSection categoryName={"Electronic"}/>
+      }
+    </Container>
+  )
 }
 
 export default ProductCartSectionDOM;
