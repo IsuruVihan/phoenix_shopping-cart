5e623c7860547780555e2b5375deef252d72033f
Sandul Renuja <sandulrenuja@gmail.com>
2021-08-17 17:12:11 +0530

    Re-implement checkout table
    Created TS type for checkout table row. Created components for formatting price value and cart delete button. Updated all styles. Re-implemented checkout table using react-bootstrap-table-next. Added pagination, sort and filter features.
    
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- src/components/Checkout/Header.tsx
+++ src/components/Checkout/Header.tsx
@@ -1,19 +1,19 @@
 import React, {FC} from 'react';
-import {Row, Col, Button} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 import {ChevronLeft} from "react-feather";
 
 const Header: FC = () => {
   return (
-    <Row className="header-row mx-sm-2">
-      <Col className="header px-0" xs={12}>
+    <Row className="header-row mx-sm-2 mt-5">
+      <Col className="header px-0 my-2" xs={12}>
         <Row className="mx-0">
-          <Col className="title px-0 py-1" md={6} sm={12} xs={12}>
+          <Col className="title px-0 py-1" xs={12} sm={12} md={9} lg={10}>
             Checkout Page
           </Col>
-          <Col className="back-btn-col px-0 py-2 py-md-0" md={6} sm={12} xs={12}>
+          <Col className="back-btn-col px-0 py-2 py-md-0 mx-0" xs={12} sm={12} md={3} lg={2}>
             <Button variant="outline-secondary back-btn" size="sm">
-              <ChevronLeft className="back-icon"/>
-              Continue Shopping
+              <ChevronLeft className="back-icon me-1"/>
+              <label>Continue Shopping</label>
             </Button>
           </Col>
         </Row>
