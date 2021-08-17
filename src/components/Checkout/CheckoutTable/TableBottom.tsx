9e7fab3cae6cea9e00dc33dec161731c18fb7b2f
SRenuuu <sandulrenuja@gmail.com>
2021-08-17 17:13:22 +0530

    Update table bottom
    Updated table bottom section to get cart total from redux store and added a field for delivery charge.
    
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- src/components/Checkout/CheckoutTable/TableBottom.tsx
+++ src/components/Checkout/CheckoutTable/TableBottom.tsx
@@ -1,32 +1,55 @@
 import React, {FC} from 'react';
 import {Col, Row} from 'react-bootstrap';
+import {useSelector} from "react-redux";
+import {State} from "../../../state";
+import FormattedPriceValue from "../../Common/FormattedPriceValue";
+import NumberFormat from "react-number-format";
 
 const TableBottom: FC = () => {
+
+  const cartData = useSelector((state: State) => state.Cart);
+  const getCartTotal = () => {
+    return cartData.reduce((a, b) => a + (parseFloat(b["amount"]) || 0), 0);
+  }
+  const cartTotal = getCartTotal();
+  const deliveryCharge = 0.0;
+
   return (
-    <Col className="table-bottom px-0" xs={12}>
-      <Row className="mx-0 px-3 pt-2">
-        <Col className="delivery-charge px-0 pt-4 pb-3" xs={12}>
-          <Row className="mx-0 px-0">
-            <Col className="px-0 delivery-charge-text" xs={6}>
-              Delivery Charge
-            </Col>
-            <Col className="px-5 delivery-charge-amount">
-              Rs. 0.00
-            </Col>
-          </Row>
-        </Col>
-        <Col className="total px-0 pt-3 pb-1" xs={12}>
-          <Row className="mx-0">
-            <Col className="px-0 total-text" xs={6}>
-              Est.Total
-            </Col>
-            <Col className="px-5 total-amount">
-              Rs. 248,770.00
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Col>
+
+    <Row className="table-bottom mx-0 px-3 pt-2 pb-2">
+      <Col className="delivery-charge px-0 pt-4 pb-3" xs={12}>
+        <Row className="mx-0 px-0">
+          <Col className="px-0 delivery-charge-text ms-3" xs={6}>
+            Delivery Charge
+          </Col>
+          <Col className="px-5 delivery-charge-amount">
+            <NumberFormat
+              value={deliveryCharge}
+              displayType={'text'}
+              thousandSeparator={true}
+              prefix={'Rs. '}
+              renderText={FormattedPriceValue}
+            />
+          </Col>
+        </Row>
+      </Col>
+      <Col className="total px-0 pt-3 pb-1" xs={12}>
+        <Row className="mx-0">
+          <Col className="px-0 total-text ms-3" xs={6}>
+            Est.Total
+          </Col>
+          <Col className="px-5 total-amount">
+            <NumberFormat
+              value={cartTotal + deliveryCharge}
+              displayType={'text'}
+              thousandSeparator={true}
+              prefix={'Rs. '}
+              renderText={FormattedPriceValue}
+            />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 }
 
