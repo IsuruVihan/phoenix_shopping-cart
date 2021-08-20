183830f4e0d12a9d53dcd14a00debe1c32578de0
SRenuuu <sandulrenuja@gmail.com>
2021-08-20 13:09:12 +0530

    Implement OrderItemsImageSet component
    Added functionality to show overlapping order images by using map function on the string array of file names, passed to the component.
    
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- src/components/Admin/Orders/OrderItemsImageSet.tsx
+++ src/components/Admin/Orders/OrderItemsImageSet.tsx
@@ -6,10 +6,16 @@ type OrderItemsImageSetProps = {
 
 const OrderItemsImageSet: FC<OrderItemsImageSetProps> = (props) => {
 
+  const renderImages = () => {
+    return props.imagePaths.map((i: string, index: number) =>
+      <div key={index} style={{backgroundImage: `url(${i})`}}/>
+    );
+  };
+
   return (
     <>
       <div className="items-image-set">
-
+        {renderImages()}
       </div>
     </>
   );
