4a1482262617bc4e3d3785f5e87fa0a9addadf26
SRenuuu <sandulrenuja@gmail.com>
2021-08-29 12:54:04 +0530

    Add confirm for remove cart-item
    Added confirmation alert when removing items from the cart in checkout table using react-confirm-alert.
    
    Signed-off-by: SRenuuu <sandulrenuja@gmail.com>

--- src/components/Common/ItemDeleteButton.tsx
+++ src/components/Common/ItemDeleteButton.tsx
@@ -1,9 +1,12 @@
 import React, {FC} from 'react';
-import {FiTrash} from "react-icons/all";
+import {FiTrash, IoCheckmark, IoClose} from "react-icons/all";
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import {CartActionCreator} from "../../state";
 import {toast} from "react-hot-toast";
+import {RemoveItem} from '../../state/action-creators/CartActionCreator';
+import {confirmAlert} from "react-confirm-alert";
+import {Button} from "react-bootstrap";
 
 type ItemDeleteButtonProps = {
   orderItemIndex: number,
@@ -16,10 +19,36 @@ const ItemDeleteButton: FC<ItemDeleteButtonProps> = (props) => {
   const {RemoveItem} = bindActionCreators(CartActionCreator, dispatch);
 
   const handleOnItemRemoveClick = (orderItemIndex: number) => {
-    RemoveItem(orderItemIndex);
-    toast.success((t) => (
-      <span><b>{orderItemName}</b> removed from cart</span>
-    ));
+    confirmAlert({
+      customUI: ({onClose}) => {
+        return (
+          <div className="remove-confirm-alert">
+            <h2 className="mt-1 mb-2">Confirm action</h2>
+            <hr/>
+
+            <div className="remove-confirm-text my-4 pt-1">
+              <span>Remove <b>{orderItemName}</b> from the cart?</span>
+            </div>
+
+            <Button className="m-2 btn-outline-danger" onClick={() => {
+              RemoveItem(orderItemIndex);
+              onClose();
+              toast.success((t) => (
+                <span><b>{orderItemName}</b> removed from cart</span>
+              ));
+            }}>
+              <IoCheckmark/> Okay
+            </Button>
+
+            <Button className="m-2 btn-outline-success" onClick={() => {
+              onClose();
+            }}>
+              <IoClose/> Cancel
+            </Button>
+          </div>
+        );
+      }
+    });
   }
 
   return (
